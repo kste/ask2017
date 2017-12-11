aa4003b81f5736b14113e163fa04f54e28b9e3bc
Stefan Koelbl <stek@dtu.dk>
2017-12-11 07:50:07 +0100

    Python script to generate ARM asm for cham128.

--- implementations/cham128.py
+++ implementations/cham128.py
@@ -1,22 +1,28 @@
 # Script to generate round function for Speck64 in ARM assembly
 
-rounds = 92
+rounds = 80
+
 # Do 4 rounds at once
 for r in range(rounds // 4):
-    print("    //Round {}".format(r))
-    print("    eor r4, r4, #2")
-    print("    eor r1, r8, r5, ror #30")
-    print("    add r4, r4, r1, ror #24")
+    print("    //Round {}".format(4*r))
+    print("    ldmia r0!, {r8-r11}")
+    print("    eor r4, r4, #{}".format(4*r))
+    print("    eor r1, r8, r5, ror #31")
+    print("    add r4, r4, r1")
+    print("    ror r4, r4, #24")
 
-    print("    eor r5, r5, #4")
+    print("    eor r5, r4, #{}".format(4*r))
     print("    eor r1, r9, r6, ror #24")
-    print("    add r5, r5, r1, ror #30")
+    print("    add r5, r5, r1")
+    print("    ror r5, r5, #31")
 
-    print("    eor r6, r6, #6")
-    print("    eor r1, r8, r7, ror #30")
-    print("    add r6, r6, r1, ror #24")
+    print("    eor r6, r6, #{}".format(4*r))
+    print("    eor r1, r10, r6, ror #31")
+    print("    add r6, r6, r1")
+    print("    ror r6, r6, #24")
 
-    print("    eor r7, r7, #8")
-    print("    eor r1, r9, r4, ror #24")
-    print("    add r7, r7, r1, ror #30")
+    print("    eor r7, r7, #{}".format(4*r))
+    print("    eor r1, r11, r4, ror #31")
+    print("    add r7, r7, r1")
+    print("    ror r7, r7, #24")            
 
